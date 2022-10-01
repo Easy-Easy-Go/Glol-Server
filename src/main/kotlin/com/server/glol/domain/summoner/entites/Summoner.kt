dc952cae864814ae0ecd6334eafa0189d8913350
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-10-02 00:55:50 +0900

    ADD Summoner Entity

---
+++ src/main/kotlin/com/server/glol/domain/summoner/entites/Summoner.kt
@@ -0,0 +1,37 @@
+package com.server.glol.domain.summoner.entites
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "summoner")
+class Summoner(
+    @Column(name = "id")
+    val id: String,
+
+    @Column(name = "account_id")
+    val accountId: String,
+
+    @Column(name = "name")
+    val name: String,
+
+    @Column(name = "puuid")
+    val puuid: String,
+
+    @Column(name = "profile_icon_id")
+    val profileIconId: Int,
+
+    @Column(name = "visited")
+    val visited: Boolean = false
+) {
+    @Id
+    @Column(name = "idx")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val idx: Long? = null
+
+    @Column(name = "summoner_id")
+    var summonerId: String = ""
+
+    fun updateSummonerId(summonerId: String) {
+        this.summonerId = summonerId
+    }
+}
