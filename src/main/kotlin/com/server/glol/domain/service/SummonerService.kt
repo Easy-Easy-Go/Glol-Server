a1876f712f64c47fab8bdd8d4c927e6ebfd3b6ac
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-10-02 00:58:30 +0900

    ADD SummonerService

--- src/main/kotlin/com/server/glol/domain/service/SummonerService.kt
+++
@@ -1,7 +0,0 @@
-package com.server.glol.domain.service
-
-import com.server.glol.domain.dto.SummonerResponseDto
-
-interface SummonerService {
-    fun getSummonerAccount(name: String): SummonerResponseDto?
-}
