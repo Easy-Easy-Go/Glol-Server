a2b20284b961637b3486de6faf56e596fa76b642
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-10-06 08:52:17 +0900

    ADD match url

--- src/main/kotlin/com/server/glol/global/config/properties/RiotProperties.kt
+++ src/main/kotlin/com/server/glol/global/config/properties/RiotProperties.kt
@@ -5,13 +5,19 @@ import org.springframework.stereotype.Component
 
 @Component
 class RiotProperties(
-    @Value("\${riot.secretKey}")
-    val secretKey: String,
+        @Value("\${riot.secretKey}")
+        val secretKey: String,
 
-    @Value("\${riot.summoner.name.uri}")
-    val summonerAPIUrl: String,
+        @Value("\${riot.summoner.name.url}")
+        val summonerAPIUrl: String,
 
-    @Value("\${riot.origin}")
-    val origin: String
+        @Value("\${riot.match.matches.uuid.url}")
+        val matchUUIDUrl: String,
+
+        @Value("\${riot.match.matches.matchId.url}")
+        val matchesMatchIdUrl: String,
+
+        @Value("\${riot.origin}")
+        val origin: String
 ) {
 }
