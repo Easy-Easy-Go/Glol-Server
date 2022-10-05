133f48c4224f76734fc1effaef0c2fa17bb0f8e7
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-10-06 08:51:58 +0900

    ADD findByPuuidByName method

--- src/main/kotlin/com/server/glol/domain/summoner/repository/SummonerCustomRepository.kt
+++ src/main/kotlin/com/server/glol/domain/summoner/repository/SummonerCustomRepository.kt
@@ -1,7 +1,9 @@
 package com.server.glol.domain.summoner.repository
 
+import com.server.glol.domain.summoner.entites.Summoner
 import com.server.glol.domain.summoner.repository.projection.SummonerVo
 
 interface SummonerCustomRepository {
     fun findSummonerByName(name: String) : SummonerVo?
+    fun findPuuidByName(name: String) : Summoner?
 }
