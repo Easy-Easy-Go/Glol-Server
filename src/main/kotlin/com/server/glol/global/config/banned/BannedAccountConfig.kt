83aaa316a957192bcedb4c7d928acfaea34bcb97
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-10-13 17:54:28 +0900

    ADD BannedAccount Config

---
+++ src/main/kotlin/com/server/glol/global/config/banned/BannedAccountConfig.kt
@@ -0,0 +1,17 @@
+package com.server.glol.global.config.banned
+
+import com.server.glol.domain.summoner.entites.Summoner
+import com.server.glol.domain.summoner.repository.SummonerRepository
+import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
+
+@Component
+class BannedAccountConfig(val summonerRepository: SummonerRepository) {
+
+    @PostConstruct
+    fun suspensionAccountSave() {
+        if (!summonerRepository.existsSummonerByName("Banned Account")) {
+            summonerRepository.save(Summoner("banned", "banned", "Banned Account", "banned", 0, true))
+        }
+    }
+}
