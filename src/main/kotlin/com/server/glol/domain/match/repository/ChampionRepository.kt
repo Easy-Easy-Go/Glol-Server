14055f12f081ae3a9daa5431af2b61bf7dcecf4f
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-10-13 18:03:19 +0900

    ADD ChampionRepository

---
+++ src/main/kotlin/com/server/glol/domain/match/repository/ChampionRepository.kt
@@ -0,0 +1,7 @@
+package com.server.glol.domain.match.repository
+
+import com.server.glol.domain.match.entities.Champion
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface ChampionRepository: JpaRepository<Champion, Long> {
+}
