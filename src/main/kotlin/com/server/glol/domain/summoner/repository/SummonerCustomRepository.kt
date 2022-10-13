7ae2d10e784a8062babf61998b2d32a1eef0b92c
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-10-13 18:08:22 +0900

    ADD 필요한 값만 반환하도록 변경

--- src/main/kotlin/com/server/glol/domain/summoner/repository/SummonerCustomRepository.kt
+++ src/main/kotlin/com/server/glol/domain/summoner/repository/SummonerCustomRepository.kt
@@ -1,9 +1,8 @@
 package com.server.glol.domain.summoner.repository
 
-import com.server.glol.domain.summoner.entites.Summoner
 import com.server.glol.domain.summoner.repository.projection.SummonerVo
 
 interface SummonerCustomRepository {
     fun findSummonerByName(name: String) : SummonerVo?
-    fun findPuuidByName(name: String) : Summoner?
+    fun findPuuidByName(name: String) : String?
 }
