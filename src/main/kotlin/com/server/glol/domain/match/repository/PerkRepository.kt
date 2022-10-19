1cd65f04675ffaa37f84aca44407f7a1ad3eb35a
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-10-19 11:48:21 +0900

    ADD PerkRepository 추가

---
+++ src/main/kotlin/com/server/glol/domain/match/repository/PerkRepository.kt
@@ -0,0 +1,9 @@
+package com.server.glol.domain.match.repository
+
+import com.server.glol.domain.match.entities.Perk
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PerkRepository: JpaRepository<Perk, Long> {
+}
