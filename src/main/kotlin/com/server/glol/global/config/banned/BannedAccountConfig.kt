dbefd17735a8c2a1ad864877a32d86b40c9045cf
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-10-21 14:58:30 +0900

    계정 정지된 소환사 정보 상수화

--- src/main/kotlin/com/server/glol/global/config/banned/BannedAccountConfig.kt
+++ src/main/kotlin/com/server/glol/global/config/banned/BannedAccountConfig.kt
@@ -8,10 +8,20 @@ import javax.annotation.PostConstruct
 @Component
 class BannedAccountConfig(val summonerRepository: SummonerRepository) {
 
+    companion object {
+        const val name = "Banned Account"
+        const val id = "bannedId"
+        const val accountId = "bannedAccountId"
+        const val puuid = "bannedPuuid"
+        const val profileIcon = 0
+        const val visited = true
+    }
     @PostConstruct
     fun suspensionAccountSave() {
-        if (!summonerRepository.existsSummonerByName("Banned Account")) {
-            summonerRepository.save(Summoner("bannedId", "bannedAccountId", "Banned Account", "bannedPuuid", 0, true))
+
+
+        if (!summonerRepository.existsSummonerByName(name)) {
+            summonerRepository.save(Summoner(id, accountId, name, puuid, profileIcon, visited))
         }
     }
 }
