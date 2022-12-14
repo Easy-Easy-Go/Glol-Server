f1749f44aed9c371227ed1fe42e88d8911423548
honghyunin <hyunin0102@gmail.com>
2022-12-14 23:01:21 +0900

    saveLeague 로직 리팩토링

--- src/main/kotlin/com/server/glol/domain/league/service/impl/LeagueServiceImpl.kt
+++ src/main/kotlin/com/server/glol/domain/league/service/impl/LeagueServiceImpl.kt
@@ -7,6 +7,7 @@ import com.server.glol.domain.league.repository.LeagueCustomRepository
 import com.server.glol.domain.league.repository.LeagueRepository
 import com.server.glol.domain.league.service.LeagueService
 import com.server.glol.domain.league.service.facade.RemoteLeagueFacade
+import com.server.glol.domain.summoner.entities.Summoner
 import com.server.glol.domain.summoner.repository.SummonerRepository
 import com.server.glol.global.exception.CustomException
 import com.server.glol.global.exception.ErrorCode.NOT_FOUND_LEAGUE
@@ -40,20 +41,42 @@ class LeagueServiceImpl(
 
     @Transactional
     override fun saveLeague(name: String) {
-        val soloLeague = leagueRepository.findLeagueBySummonerNameAndQueueType(name, SOLO_RANK)
-            ?: League()
-        val freeLeague = leagueRepository.findLeagueBySummonerNameAndQueueType(name, FREE_RANK)
-            ?: League()
+        val soloLeague = leagueCustomRepository.getLeague(name, SOLO_RANK) ?: League()
+        val freeLeague = leagueCustomRepository.getLeague(name, FREE_RANK) ?: League()
+        val findSummoner = summonerRepository.findSummonerByName(name)!!
+        val getLeague = getLeagueByName(findSummoner.name)
 
-        val findSummoner = summonerRepository.findSummonerByName(name)
+        updateSummonerId(getLeague, findSummoner)
 
-        getLeague(name).forEach { league ->
-            if (league.queueType == SOLO_RANK)
-                soloLeague.leagueUpdate(league, findSummoner)
-            if (league.queueType == FREE_RANK)
-                freeLeague.leagueUpdate(league, findSummoner)
-        }
+        leagueUpdate(soloLeague, freeLeague, findSummoner, getLeague)
 
         leagueRepository.saveAll(mutableSetOf(soloLeague, freeLeague))
+    }
+
+    private fun updateSummonerId(leagueDto: MutableSet<LeagueDto>, findSummoner: Summoner) {
+        leagueDto.elementAt(0).let { findSummoner.updateSummonerId(it.summonerId) }
+    }
+
+    private fun isExistsLeague(name: String): Boolean = leagueRepository.existsBySummonerName(name)
+
+    private fun isExistsSummoner(name: String): Boolean = summonerRepository.existsSummonerByName(name)
+
+    private fun getLeagueByName(name: String): MutableSet<LeagueDto> =
+        if (isExistsSummoner(name)) {
+            summonerRepository.findSummonerByName(name)
+                .let { summoner -> remoteLeagueFacade.getLeague(summoner!!.id) }
+        } else {
+            log.debug("${NOT_FOUND_SUMMONER.msg} in getLeagueByName")
+            throw CustomException(NOT_FOUND_SUMMONER)
+        }
+
+    private fun leagueUpdate(
+        soloLeague: League,
+        freeLeague: League,
+        findSummoner: Summoner,
+        league: MutableSet<LeagueDto>
+    ) {
+        soloLeague.leagueUpdate(league.elementAt(0), findSummoner)
+        freeLeague.leagueUpdate(league.elementAt(1), findSummoner)
     }
 }
